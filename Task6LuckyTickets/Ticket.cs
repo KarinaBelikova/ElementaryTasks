e45c3d1a85c5a20c2b23735516acb00a53769859
Karina Belikova <kiit15.belikova@gmail.com>
2019-06-04 20:49:49 +0300

    last renaming

---
+++ Task6LuckyTickets/Ticket.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6LuckyTickets
+{
+    public class Ticket
+    {
+        private int[] _numbers;
+
+        public int Length { get => _numbers.Length; }
+        public ILuckyAlgorithm Algorithm { get; set; }
+
+        public int this[int index]
+        {
+            get
+            {
+                return _numbers[index];
+            }
+            set
+            {
+                _numbers[index] = value;
+            }
+        }
+
+        public Ticket(int[] numbers, ILuckyAlgorithm algorithm)
+        {
+            _numbers = numbers;
+            Algorithm = algorithm;
+        }
+
+        public bool IsLucky()
+        {
+            return Algorithm.isLuckyTicket(this);
+        }
+    }
+}
